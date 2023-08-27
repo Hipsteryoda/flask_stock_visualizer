af930c06ae151a18c271b7ca91828b73d4ac0c2a
Hipsteryoda <ksmith952913@gmail.com>
2023-08-27 10:47:34 -0700

    feat: added option to pass starting id to iterate from

--- web/optimization_params_update.py
+++ web/optimization_params_update.py
@@ -1,11 +1,19 @@
 from mv_avg_window_optimizer import Optimized_Symbol, add_lag_price
 import pandas as pd
 from tqdm import tqdm
-
+import sys
 
 stocks = pd.read_csv('/home/ksmith/flask_stock_visualizer/web/nasdaq_screener_1690226089920.csv')
 
 stock_list = stocks.Symbol.to_list()
 
-for symbol in stock_list:
-    Optimized_Symbol(symbol)
+start_idx = int(sys.argv[1])
+
+if start_idx != None:
+    for symbol in tqdm(stock_list[start_idx:]):
+        Optimized_Symbol(symbol)
+            
+else:
+    for symbol in tqdm(stock_list):
+        Optimized_Symbol(symbol)
+            
